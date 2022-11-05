d9f3e66ea7203eea9547203366cf64fd116761c9
김현석 <hyeonsuk@gimhyeonseog-ui-MacBookAir.local>
2022-11-05 15:17:52 +0900

    first commit

---
+++ src/main/java/com/herewego/herewegoapi/HerewegoapiApplication.java
@@ -0,0 +1,13 @@
+package com.herewego.herewegoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HerewegoapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HerewegoapiApplication.class, args);
+	}
+
+}
