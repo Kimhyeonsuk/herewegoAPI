d8fd86fb5d64d2fc107b8350361859b619c28b50
김현석 <hyeonsuk@gimhyeonseog-ui-MacBookAir.local>
2022-11-06 14:03:26 +0900

    [DEVELOP] feat : testController 작성, 구동 확인

--- src/main/java/com/herewego/herewegoapi/controller/TestController.java
+++ src/main/java/com/herewego/herewegoapi/controller/TestController.java
@@ -3,10 +3,13 @@ package com.herewego.herewegoapi.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class TestController {
-    @RequestMapping(value = "/")
+
+    @RequestMapping(value = "/test", method = RequestMethod.GET)
     public String testMethod () {
         return "Hello here we go";
     }
