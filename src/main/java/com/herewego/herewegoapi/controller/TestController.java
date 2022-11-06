d393a67bdfd6d949e3a6ced2ed8d1944cd0663a0
김현석 <hyeonsuk@gimhyeonseog-ui-MacBookAir.local>
2022-11-06 13:02:34 +0900

    [DEVELOP] feat: dependency 설정, test controller 작성

---
+++ src/main/java/com/herewego/herewegoapi/controller/TestController.java
@@ -0,0 +1,13 @@
+package com.herewego.herewegoapi.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class TestController {
+    @RequestMapping(value = "/")
+    public String testMethod () {
+        return "Hello here we go";
+    }
+}
