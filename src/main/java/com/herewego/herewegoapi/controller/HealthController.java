69f69d0c4506924dd7cb154a4aad01af08e21bd3
unknown <philippe10@naver.com>
2022-11-29 09:24:25 +0900

    [HWG-3] feat : 코드 수정

--- src/main/java/com/herewego/herewegoapi/controller/HealthController.java
+++ src/main/java/com/herewego/herewegoapi/controller/HealthController.java
@@ -1,13 +1,15 @@
 package com.herewego.herewegoapi.controller;
 
 import com.herewego.herewegoapi.response.ApiResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class HealthController {
     @GetMapping(value = "/health")
-    public Object health()  {
+    public ResponseEntity<?> health()  {
 
-        return ApiResponse.ok();
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 }
