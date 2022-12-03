70702af8672b7d35c981cb3e3245ab09f3101fd6
unknown <philippe10@naver.com>
2022-12-03 18:15:16 +0900

    [HWG-3] feat : 코드 수정

--- src/main/java/com/herewego/herewegoapi/controller/TestController.java
+++ src/main/java/com/herewego/herewegoapi/controller/TestController.java
@@ -1,5 +1,6 @@
 package com.herewego.herewegoapi.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,8 +10,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestController {
 
+    @Value("${spring.profiles.active}")
+    String profiles;
+
     @RequestMapping(value = "/test", method = RequestMethod.GET)
     public String testMethod () {
-        return "Hello here we go";
+        return profiles;
     }
 }
