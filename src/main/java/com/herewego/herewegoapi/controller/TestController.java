2513dfffddbbbd62770652537e9550c38e65c9c3
unknown <philippe10@naver.com>
2022-12-24 11:46:16 +0900

    [HWG-8] feat: route53등록을 위한 url 예외 처리

--- src/main/java/com/herewego/herewegoapi/controller/TestController.java
+++ src/main/java/com/herewego/herewegoapi/controller/TestController.java
@@ -1,5 +1,8 @@
 package com.herewego.herewegoapi.controller;
 
+import com.herewego.herewegoapi.security.jwt.JwtAuthenticationFilter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -7,12 +10,14 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
+    private static final Logger LOGGER = LoggerFactory.getLogger(TestController.class);
 
     @Value("${spring.profiles.active}")
     String profiles;
 
     @RequestMapping(value = "/test", method = RequestMethod.GET)
     public String testMethod () {
+        LOGGER.debug("/test uri 요청 profile: {}", profiles);
         return profiles;
     }
 }
