6e44b505cf4a4c0deec71a5c35f4e7454a143091
unknown <philippe10@naver.com>
2022-12-27 15:48:39 +0900

    [HWG-10] fix: authorization insert 수정

--- src/main/java/com/herewego/herewegoapi/security/oauth/CustomOAuth2UserService.java
+++ src/main/java/com/herewego/herewegoapi/security/oauth/CustomOAuth2UserService.java
@@ -90,5 +90,6 @@ public class CustomOAuth2UserService extends DefaultOAuth2UserService {
                 .email(userInfo.getEmail())
                 .authProvider(authProvider)
                 .build();
+        authorizationRepository.save(authorization);
     }
 }
