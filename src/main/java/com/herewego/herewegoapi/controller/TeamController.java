98dbf5caea3bdb7ee71ed9918d86e14dc949960e
unknown <philippe10@naver.com>
2023-01-04 21:12:11 +0900

    [HWG-15] feat: favorite team 추가 API 틀 작성

--- src/main/java/com/herewego/herewegoapi/controller/TeamController.java
+++ src/main/java/com/herewego/herewegoapi/controller/TeamController.java
@@ -1,13 +1,19 @@
 package com.herewego.herewegoapi.controller;
 
 
+import com.herewego.herewegoapi.model.request.UpdateFavoriteTeamVO;
 import com.herewego.herewegoapi.response.ApiResponse;
+import com.herewego.herewegoapi.service.TeamService;
 import com.herewego.herewegoapi.vo.RegisterUserVO;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value = "/v1")
 public class TeamController {
+    @Autowired
+    TeamService teamService;
+
     @GetMapping(value = "/teams")
     public Object getUserInformation(
             @RequestHeader(value = "Account-Token") String accountToken,
@@ -16,5 +22,16 @@ public class TeamController {
             @PathVariable("league") String leagueName)  {
 
         return ApiResponse.ok();
+    }
+
+    @PutMapping(value = "/favorites")
+    public Object updateFavoriteTeam(
+            @RequestHeader(value = "Authorization") String accountToken,
+            @RequestHeader(value = "Email") String email,
+            @RequestBody UpdateFavoriteTeamVO updateFavoriteTeamVO) {
+
+        teamService.updateFavoriteTeam(updateFavoriteTeamVO.getTeamName(), updateFavoriteTeamVO.getLeagueName());
+        return ApiResponse.ok();
+
     }
 }
---
+++ src/main/java/com/herewego/herewegoapi/service/TeamService.java
@@ -0,0 +1,12 @@
+package com.herewego.herewegoapi.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class TeamService {
+
+    public void updateFavoriteTeam(String teamName, String leagueName) {
+        
+    }
+}
